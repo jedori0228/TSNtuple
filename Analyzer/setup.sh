7967011df80cc5c0aa72282d84390e4f9df357e7
Kyeongpil Lee <kplee@cern.ch>
2017-09-25 02:32:21 +0900

    enable cmsenv in tamsa2

--- Analyzer/setup.sh
+++ Analyzer/setup.sh
@@ -12,7 +12,17 @@ export ROOT_INCLUDE_PATH=${KP_ANALYZER_PATH}:${ROOT_INCLUDE_PATH}
 
 # -- ntuple path -- #
 export KP_DATA_PATH=""
-if [ $HOSTNAME == "tamsa2.snu.ac.kr" ]; # -- 147.47.242.67 -- # 
+if [ $HOSTNAME == "tamsa2.snu.ac.kr" ] || [ "compute-"=~"$HOSTNAME" ] ; # -- 147.47.242.67 -- # 
 then 
 	KP_DATA_PATH="/data5/Users/kplee/TriggerStudyNtuple"
+
+	# -- cmssw setup -- #
+	export SCRAM_ARCH=slc6_amd64_gcc530
+	export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+	source $VO_CMS_SW_DIR/cmsset_default.sh
+
+	cd /share_home/kplee/TagProbe/v20170815_1st_MuonPOG_92Xv2_TrkIter/CMSSW_9_2_4/src
+	eval `scramv1 runtime -sh` # -- equivalent to cmsenv (cmsenv doesn't work. why?) -- #
+	cd $KP_ANALYZER_PATH
+
 fi
