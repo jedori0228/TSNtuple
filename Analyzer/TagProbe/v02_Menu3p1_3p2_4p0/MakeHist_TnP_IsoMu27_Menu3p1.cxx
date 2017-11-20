ffad961c8a628dfde417ff2919f2de8580894348
Kyoengpil Lee <kplee@cern.ch>
2017-11-21 02:37:50 +0900

    comparison of menu 3.1, 3.2 and 4.0

---
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_IsoMu27_Menu3p1.cxx
@@ -0,0 +1,20 @@
+#include <TagProbe/MakeHist_TnP_IsoMu27.h>
+
+void MakeHist_TnP_IsoMu27_Menu3p1()
+{
+	HistProducer* producer = new HistProducer();
+	producer->SetOutputFileName( "ROOTFile_MakeHist_TnP_IsoMu27_Menu3p1.root" );
+
+	TString DataPath = gSystem->Getenv("KP_DATA_PATH");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics1_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics2_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics3_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics4_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics5_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics6_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics7_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics8_Run2017Fv1/ntuple_*.root");
+	producer->Produce();
+	// producer->AddDataPath("");
+	// producer->AddDataPath("");
+}
