8f415fbbc76b402ec50d42483b37e2b04766c113
Kyoengpil Lee <kplee@cern.ch>
2017-11-21 02:59:53 -0500

    try to recover

--- Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_IsoMu27_Menu3p1.cxx
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_IsoMu27_Menu3p1.cxx
@@ -6,14 +6,14 @@ void MakeHist_TnP_IsoMu27_Menu3p1()
 	producer->SetOutputFileName( "ROOTFile_MakeHist_TnP_IsoMu27_Menu3p1.root" );
 
 	TString DataPath = gSystem->Getenv("KP_DATA_PATH");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics1_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics2_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics3_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics4_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics5_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics6_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics7_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics8_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics1_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics2_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics3_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics4_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics5_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics6_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics7_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu3p1_EphemeralHLTPhysics8_Run2017Dv1/ntuple_*.root");
 	producer->Produce();
 	// producer->AddDataPath("");
 	// producer->AddDataPath("");
--- Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_IsoMu27_Menu4p0.cxx
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_IsoMu27_Menu4p0.cxx
@@ -6,14 +6,14 @@ void MakeHist_TnP_IsoMu27_Menu4p0()
 	producer->SetOutputFileName( "ROOTFile_MakeHist_TnP_IsoMu27_Menu4p0.root" );
 
 	TString DataPath = gSystem->Getenv("KP_DATA_PATH");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics1_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics2_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics3_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics4_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics5_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics6_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics7_Run2017Fv1/ntuple_*.root");
-	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics8_Run2017Fv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics1_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics2_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics3_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics4_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics5_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics6_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics7_Run2017Dv1/ntuple_*.root");
+	producer->AddDataPath(DataPath+"/92X/TSntuple_v20171109_Menu4p0_EphemeralHLTPhysics8_Run2017Dv1/ntuple_*.root");
 	producer->Produce();
 	// producer->AddDataPath("");
 	// producer->AddDataPath("");
