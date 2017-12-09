6a318664b8e43c600938d8a5f79279d2537fde97
Kyoengpil Lee <kplee@cern.ch>
2017-12-09 13:43:12 +0900

    for L1/offline eff.

---
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/MakeHist_TnP_L1SingleMu22.cxx
@@ -0,0 +1,38 @@
+#include <TagProbe/MakeHist_TnP_SingleMu22.h>
+
+void Setup( HistProducer* producer, TString dataType );
+
+void MakeHist_TnP_SingleMu22(TString dataType)
+{
+	HistProducer* producer = new HistProducer();
+	Setup( producer, dataType );
+
+	producer->Produce();
+}
+
+void Setup( HistProducer* producer, TString dataType )
+{
+	TString dataPath = gSystem->Getenv("KP_DATA_PATH");
+
+	if( dataType == "Menu3p1" )
+	{
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p1.root" );
+
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171128_SingleMuon_Run2017Dv1_302635_to_302635_GoldenJSON_Menu3p1/ntuple_*.root");
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171129_SingleMuon_Run2017Dv1_302525_to_302634_GoldenJSON_Menu3p1/ntuple_*.root");
+	}
+	else if( dataType == "Menu3p2" )
+	{
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2.root" );
+
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171128_SingleMuon_Run2017Dv1_302635_to_302635_GoldenJSON_Menu3p2/ntuple_*.root");
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171129_SingleMuon_Run2017Dv1_302525_to_302634_GoldenJSON_Menu3p2/ntuple_*.root");
+	}
+	else if( dataType == "Menu4p0" )
+	{
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0.root" );
+
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171128_SingleMuon_Run2017Dv1_302635_to_302635_GoldenJSON_Menu4p0/ntuple_*.root");
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171129_SingleMuon_Run2017Dv1_302525_to_302634_GoldenJSON_Menu4p0/ntuple_*.root");
+	}
+}
---
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/script_MakeHist_TnP_L1SingleMu22.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+root -b -q 'MakeHist_TnP_L1SingleMu22.cxx++("Menu3p1")' >&log_MakeHist_TnP_L1SingleMu22_Menu3p1
+
+root -b -q 'MakeHist_TnP_L1SingleMu22.cxx++("Menu3p2")' >&log_MakeHist_TnP_L1SingleMu22_Menu3p2
+
+root -b -q 'MakeHist_TnP_L1SingleMu22.cxx++("Menu4p0")' >&log_MakeHist_TnP_L1SingleMu22_Menu4p0
+
+echo "finished"
