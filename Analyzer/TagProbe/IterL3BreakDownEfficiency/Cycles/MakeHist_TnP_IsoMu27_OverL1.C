803a0770480398a25ef85fcd811d783e7f194b02
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-02-12 08:01:15 +0900

    updated

--- Analyzer/TagProbe/IterL3BreakDownEfficiency/Cycles/MakeHist_TnP_IsoMu27_OverL1.C
+++ Analyzer/TagProbe/IterL3BreakDownEfficiency/Cycles/MakeHist_TnP_IsoMu27_OverL1.C
@@ -1,17 +1,15 @@
 #include <TagProbe/MakeHist_TnP_IsoMu27_OverL1.h>
 
-void MakeHist_TnP_IsoMu27_OverL1()
+void MakeHist_TnP_IsoMu27_OverL1(TString infile, TString outfile)
 {
-
-  TString dataPath = "/data8/Users/jskim/MuonHLTNtuple/171218/merged.root";
 
   HistProducer* producer = new HistProducer();
 
   producer->L1MuonPtCut = 22.;
   producer->OffLineMuonPtCut = 29.;
-  producer->L3MuonPtCut = 27.;
-  producer->SetOutputFileName( "/data8/Users/jskim/CMSSW_9_2_14/src/TSNtuple/Analyzer/TagProbe/IterL3BreakDownEfficiency/Outputs/Output_IsoMu27_OverL1.root" );
-  producer->AddDataPath(dataPath);
+  producer->L3MuonPtCut = 0.;
+  producer->SetOutputFileName(outfile);
+  producer->AddDataPath(infile);
   producer->Produce();
 }
 
