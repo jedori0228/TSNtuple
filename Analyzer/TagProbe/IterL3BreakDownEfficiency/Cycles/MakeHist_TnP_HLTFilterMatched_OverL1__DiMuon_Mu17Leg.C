3f03bb4b3d5391fa3e8ec0d0c0976e5af0b3e2d4
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-03-20 15:13:23 +0900

    DiMuon TnP code added

---
+++ Analyzer/TagProbe/IterL3BreakDownEfficiency/Cycles/MakeHist_TnP_HLTFilterMatched_OverL1__DiMuon_Mu17Leg.C
@@ -0,0 +1,26 @@
+#include <TagProbe/MakeHist_TnP_HLTFilterMatched_AND_OverL1.h>
+
+void MakeHist_TnP_HLTFilterMatched_OverL1__DiMuon_Mu17Leg(TString infile, TString outfile)
+{
+
+  HistProducer* producer = new HistProducer();
+
+  producer->L1MuonPtCut = 5.;
+  producer->L1MuonQual = 8;
+
+  producer->L3MuonPtCut = 0.;
+
+  producer->OffLineMuonPtCut = 19.;
+
+  vector<TString> filtersAND = {
+    "hltDiMuon178RelTrkIsoFiltered0p4",
+    "hltL3fL1DoubleMu155fFiltered17",
+  };
+
+  producer->PassingProbeHLTFilter = filtersAND;
+  producer->SetOutputFileName(outfile);
+  producer->AddDataPath(infile);
+  producer->Produce();
+
+}
+
